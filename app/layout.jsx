6804028a29e639537ca69f03c5be374af72de643
gokce <gokcetascii@gmail.com>
2024-02-22 22:05:24 +0300

    header and footer added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
-import "../styles/reset.css"
-import "../styles/global.css"
+import "@/styles/reset.css";
+import "@/styles/global.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
@@ -14,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" classname={inter.className}>
-      <body classname="container">{children}</body>
+      <body classname="container">
+        <Header/>
+        <main>{children}</main>
+        <Footer/>
+        </body>
     </html>
   );
 }
