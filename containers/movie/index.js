78d9ffbc35c0228cce1ac54ec1be20542bfc0825
gokce <gokcetascii@gmail.com>
2024-02-24 00:08:48 +0300

    movie container index page created and added content

---
+++ containers/movie/index.js
@@ -0,0 +1,13 @@
+import { FeaturedMovie } from '@/components/featured-movie'
+import React from 'react'
+
+
+function MovieContainer({movie}) {
+  return (
+    <div>
+        <FeaturedMovie movie={movie} isCompact={false}/>
+    </div>
+  )
+}
+
+export default MovieContainer
