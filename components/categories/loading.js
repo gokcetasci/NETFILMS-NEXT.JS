0dc9d8b4d523c3525e5aee4c33999f0e6f8cc6e6
gokce <gokcetascii@gmail.com>
2024-02-24 00:49:51 +0300

    categories loading created and content, styles added

---
+++ components/categories/loading.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Skeleton from '../skeleton'
+import styles from "./styles.module.css"
+
+function CategoriesLoading() {
+  return (
+    <div className={styles.categories}>
+        {Array(5)
+            .fill
+            .map((_, index) => (
+                <Skeleton key={index} height={72}/>
+            ))
+        }
+    </div>
+  )
+}
+
+export default CategoriesLoading
