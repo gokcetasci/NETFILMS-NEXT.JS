0b853f74e09ff1237d0f9c58174aacb05e77a318
gokce <gokcetascii@gmail.com>
2024-02-26 17:02:04 +0300

    edited

--- app/[[...category]]/page.js
+++ app/[[...category]]/page.js
@@ -1,60 +1,37 @@
 import React from "react";
-import HomeContainer from "@/containers/home"; 
-
-
-const API_URL = "https://api.themoviedb.org/3"
-
-const getTopRatedMovies = async () => {
-    const res = await fetch (`${API_URL}/movie/top_rated?api_key=${process.env.API_KEY}&page=1`)
-
-    return await res.json();
-}
-
-const getPopularMovies = async () => {
-    const res = await fetch (`${API_URL}/movie/popular?api_key=${process.env.API_KEY}&page=1`)
-
-    return await res.json();
-}
-
-const getCategories = async () => {
-    const res = await fetch (`${API_URL}/genre/movie/list?api_key=${process.env.API_KEY}&page=1`)
-
-    return await res.json();
-}
-
-const getSingleCategory = async (genreId) => {
-    const res = await fetch (`${API_URL}/discover/movie?api_key=${process.env.API_KEY}&with_genres=${genreId}`)
-
-    return await res.json();
-}
+import HomeContainer from "@/containers/home";
+import {
+  getTopRatedMovies,
+  getPopularMovies,
+  getCategories,
+  getSingleCategory,
+} from "@/services/movie";
 
 async function HomePage({ params }) {
+  let selectedCategory;
 
-    let selectedCategory;
+  const [
+    { results: topRatedMovies },
+    { results: popularMovies },
+    { genres: categories },
+  ] = await Promise.all([getTopRatedMovies(), getPopularMovies(), getCategories()]);
 
-    const topRatedPromise = getTopRatedMovies (); 
-    const popularPromise = getPopularMovies();
-    const categoriesPromise = getCategories();
+  if (params.category?.length > 0) {
+    const { results } = await getSingleCategory(params.category[0]);
+    selectedCategory = results;
+  }
 
-    const [{ results: topRatedMovies}, {results: popularMovies}, {genres: categories}] = await Promise.all([topRatedPromise, popularPromise, categoriesPromise]);
-
-
-    if (params.category?.length > 0) {
-        const { results } = await getSingleCategory(params.category[0]);
-        selectedCategory =  results;
-    }
-
-    return (
-        <HomeContainer 
-            topRatedMovies={topRatedMovies}
-            popularMovies={popularMovies}
-            categories={categories}
-            selectedCategory={{
-                id: params.category?.[0] ?? "",
-                movies: selectedCategory ? selectedCategory.slice(0, 7) : [],
-            }}
-        />
-    );
+  return (
+    <HomeContainer
+      topRatedMovies={topRatedMovies}
+      popularMovies={popularMovies}
+      categories={categories}
+      selectedCategory={{
+        id: params.category?.[0] ?? "",
+        movies: selectedCategory ? selectedCategory.slice(0, 7) : [],
+      }}
+    />
+  );
 }
 
 export default HomePage;
--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -1,13 +1,7 @@
 import React from "react";
 import { notFound } from "next/navigation";
 import MovieContainer from "@/containers/movie";
-
-const API_URL = "https://api.themoviedb.org/3"
-
-const getMovie = async (movieId) => {
-    const res = await fetch (`${API_URL}/movie/${movieId}?api_key=${process.env.API_KEY}&page=1`);
-    return await res.json();
-}
+import {getMovie} from "@/services/movie"
 
 async function MoviePage({ params, searchParams }) {
 
@@ -15,11 +9,7 @@ async function MoviePage({ params, searchParams }) {
 
   if (!movieDetail) {
     notFound();
-  } //movie detail olmadığında kullanıcıyı 404 sayfasına yönlendir
-
-  if (searchParams.error === "true") {
-    throw new Error("Error happened");
-  }
+  } 
 
   return (
     <div>
